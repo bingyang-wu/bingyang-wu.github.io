d71020359c776ab2c377d673402492be5393588c
wubingyang <bingyangwu.cn@gmail.com>
2020-06-18 14:28:40 +0800

    delete

--- example/docs/.vuepress/plugins/element/index.js
+++
@@ -1,8 +0,0 @@
-const { path } = require('@vuepress/shared-utils')
-
-module.exports = (options, context) => ({
-  name: '@DuebassLei/vuepress-plugin-element-ui',
-  enhanceAppFiles: [
-    path.resolve(__dirname, './bin/enhanceAppFile.js')
-  ]
-})
